32ced5cbf52d5ee758d5644529a0696f6c66b6da
chrishughandrew <72550656+chrishughandrew@users.noreply.github.com>
2024-08-14 12:34:14 +0100

    Add some Program statements to run in the console.

--- MatchGame/Program.cs
+++ MatchGame/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Playing Match!");
+using MatchGame;
+
+//Take some console readline input for matchRule and number of packs of cards.
+// (hardcode for now)
+int numofPacks = 3;
+MatchRuleEnum chosenRule = MatchRuleEnum.SUIT;
+
+// Instantiate and Setup Game with given parameters
+Game game = new Game(new Deck(), new Player(), new Player());
+
+game.Setup(chosenRule, numofPacks);
+
+// Play and Delcare the winner. 
+game.Play();
+Console.WriteLine($"The winner is: {game.DeclareWinner()}");
+
+
+
